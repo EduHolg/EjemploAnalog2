820fc322b45aa3a3c2e27e24ad30f6e2ddf97293
adennen <aron@arondennen.com>
2024-05-20 21:40:01 -0400

    Finished part 7 - design submission

--- docs/info.md
+++ docs/info.md
@@ -9,12 +9,13 @@ You can also include images in this folder and reference them in the markdown. E
 
 ## How it works
 
-Explain how your project works
+This is the double inverter project from Matt's analog course,
+it's two inverters (one big and one small) connected for form a buffer
 
 ## How to test
 
-Explain how to use your project
+Analog input is ua[1], output is ua[0]
 
 ## External hardware
 
-List external hardware used in your project (e.g. PMOD, LED display, etc), if any
+none
--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_adennen_inverter (
     input  wire       VGND,
     input  wire       VPWR,
     input  wire [7:0] ui_in,    // Dedicated inputs
@@ -18,5 +18,40 @@ module tt_um_example (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
+
+  inverter inverter(
+	  .VDD(VPWR),
+	  .VSS(VGND),
+	  .IN(ua[1]),
+	  .OUT(ua[0])
+  );
+
+  assign uo_out[0] = VGND;
+  assign uo_out[1] = VGND;
+  assign uo_out[2] = VGND;
+  assign uo_out[3] = VGND;
+  assign uo_out[4] = VGND;
+  assign uo_out[5] = VGND;
+  assign uo_out[6] = VGND;
+  assign uo_out[7] = VGND;
+
+  assign uio_oe[0] = VGND;
+  assign uio_oe[1] = VGND;
+  assign uio_oe[2] = VGND;
+  assign uio_oe[3] = VGND;
+  assign uio_oe[4] = VGND;
+  assign uio_oe[5] = VGND;
+  assign uio_oe[6] = VGND;
+  assign uio_oe[7] = VGND;
+
+  assign uio_out[0] = VGND;
+  assign uio_out[1] = VGND;
+  assign uio_out[2] = VGND;
+  assign uio_out[3] = VGND;
+  assign uio_out[4] = VGND;
+  assign uio_out[5] = VGND;
+  assign uio_out[6] = VGND;
+  assign uio_out[7] = VGND;
+
 
 endmodule
